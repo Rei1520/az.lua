d423a970f0438222899be80d163d1a333cc745ca
Rei1520 <130766351+Rei1520@users.noreply.github.com>
2023-04-14 22:28:35 +0900

    Update az.lua

--- az.lua
+++ az.lua
@@ -5,7 +5,5 @@ if PlaceId == 2753915549 or PlaceId == 4442272183 or PlaceId == 7449423635 then
 elseif PlaceId == 4520749081 or PlaceId == 6381829480 or PlaceId == 5931540094 then
     loadstring(game:HttpGet"https://raw.githubusercontent.com/xQuartyx/DonateMe/main/KL")()
 elseif PlaceId == 537413528 then
-    loadstring(game:HttpGet"https://raw.githubusercontent.com/xQuartyx/DonateMe/main/BABFT")()
-elseif PlaceId == 8396586868 or PlaceId == 9432106399 then
 
 end
