cc496db62801e07d682ae1e9f5b9b1b8a8b3e7f8
Rei1520 <130766351+Rei1520@users.noreply.github.com>
2023-07-09 12:28:35 +0900

    Update chat.lua

--- chat.lua
+++ chat.lua
@@ -117,50 +117,23 @@ minimze.TextColor3 = Color3.fromRGB(0, 0, 0)
 minimze.TextSize = 25.000
 
 -- Scripts:
+-- Settings
 
-local function HDPTQN_fake_script() -- X.LocalScript 
-	local script = Instance.new('LocalScript', X)
+local message = "" -- the message
+local Tiemlolol = 5000000000 -- the wait time between chats, anything under 2 isnt recommended
 
-	script.Parent.MouseEnter:Connect(function()
-		script.Parent.BackgroundColor3 = Color3.fromRGB(200, 0, 8)
-		script.Parent.BorderColor = Color3.fromRGB(200, 0, 8)
-		script.Parent.TextColor3 = Color3.fromRGB(255, 255, 255)
-	end)
-	script.Parent.MouseLeave:Connect(function()
-		script.Parent.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-		script.Parent.BorderColor = Color3.fromRGB(255, 255, 255)
-		script.Parent.TextColor3 = Color3.fromRGB(0, 0, 0)
-	end)
+
+-- Script (dont modify)
+
+if _G.AutoSay then _G.AutoSay = false
+else _G.AutoSay = true
 end
-coroutine.wrap(HDPTQN_fake_script)()
-local function CDWNJ_fake_script() -- X.LocalScript 
-	local script = Instance.new('LocalScript', X)
-
-	local btn = script.Parent
-	btn.MouseButton1Click:Connect(function()
-		script.Parent.Parent.Parent:Destroy()
-	end)
+while _G.AutoSay do
+	local args = {[1] = message,[2] = "All"}
+	game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+	task.wait(tonumber(Tiemlolol))
 end
-coroutine.wrap(CDWNJ_fake_script)()
-local function DJZDV_fake_script() -- ChatBtn.LocalScript 
-	local script = Instance.new('LocalScript', ChatBtn)
 
-	local txtbox = script.Parent.Parent.Chat
-local delay = script.Parent.Parent.DelayBox
-local btn = script.Parent
-btn.MouseButton1Click:Connect(function()
-    local keywords = { "a", "i", "cock", "fuck", "asshole", "rape", "boobs", "titties", "sex", "hoe", "porn", "nigga", "penis", "beaner", "thot", "xxx", "hentai", "bitch", "bitches", "shit", "kys", "butthole", "masturbate", "big black cock", "blowjob", "bites your cock", "cock sucker", "fat ass hoe" } --Keyword
-    local replace = { "あ", "い", "сосk", "fu𐌑𝖔 ͜ ͣ c𐌑ḳ", "аѕѕһоlе", "rаре", "Вoobs", "Тitties", "s⁥⁥⁥⁥⁥⁥ех", "һое", "рогn", "п⁥iɡ⁥ɡ⁥а", "реп⁥is", "bеаn⁥ег", "ТН⁥⁥⁥⁥⁥ОТ", "ΧΧΧ", "НЕΝТАӀ", "ВI⁥⁥⁥⁥⁥⁥TСН", "ВIT⁥⁥⁥⁥⁥⁥⁥СНEЅ", "ЅНIТ", "К⁥⁥⁥⁥⁥YЅ", "ВUТТ⁥⁥⁥⁥НOLЕ", "МАЅТURВАТЕ", "big black со⁥⁥⁥сk", "Вӏоwjob", "Віtеѕ уоur сосk", "сосk suсkеr", "f⁥аt а⁥ѕѕ һое" } --Replace
-    local message = string.lower(txtbox.Text)
-    for word in string.gmatch(message, "%S+") do
-        for i, key in ipairs(keywords) do
-            if word == string.lower(key) then
-                word = replace[i]
-                break
-            end
-        end
-        game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(word, "All")
-        wait(tonumber(delay.Text))
     end
 end)
 end
